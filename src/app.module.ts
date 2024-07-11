ecb77c441766d45fc5490becc95fd15a80d0041f
Amrit Raj <amrit.raj1504@gmail.com>
2024-07-11 21:14:42 +0530

    config changed

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,12 @@ import { Module } from '@nestjs/common';
 import { DatabaseModule } from './database/database.module';
 import { TodoModule } from './todo/todo.module';
 import { AuthModule } from './auth/auth.module';
-import { RootController } from './app.controller';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 
 @Module({
   imports: [DatabaseModule, TodoModule, AuthModule],
-  controllers: [RootController],
-  providers: [],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
