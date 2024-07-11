031f8de1400f53cd67184a3ab70a8b9f64319f41
Amrit Raj <amrit.raj1504@gmail.com>
2024-07-11 21:06:05 +0530

    config changed

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,14 +1,13 @@
 import { Controller, Get } from '@nestjs/common';
-import { ApiOperation, ApiTags } from '@nestjs/swagger';
+import { AppService } from './app.service';
 
-@ApiTags("Root")
+
 @Controller()
-export class RootController {
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
   @Get()
-  @ApiOperation({
-    summary: 'Root Endpoint',
-  })
-  root() {
-    return { message: 'Welcome to the Todo Application API Using NestJS', Author: 'Amrit Raj' };
+  getHello(): string {
+    return this.appService.getHello();
   }
 }
